2e5b44e5f1c02c6688713981ca2f361764966c4e
ChristinaGaitan <christina.gaitan@gmail.com>
2020-05-15 15:11:17 -0500

    First commit

---
+++ app/src/main/java/com/lcgt/instagramcloneparse/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lcgt.instagramcloneparse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
